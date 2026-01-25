6506395587628f9db30f6019c625f0cfcd1431e7
xiaoheid-dunkel <2477636849@qq.com>
2026-01-25 11:50:58 +0800

    Initial commit: My Engine

---
+++ Hazel-ScriptCore/Source/Hazel/Scene/Components.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hazel
+{
+	public abstract class Component
+	{
+		public Entity Entity { get; internal set; }
+	}
+
+	public class TransformComponent : Component
+	{
+		public Vector3 Translation
+		{
+			get
+			{
+				InternalCalls.TransformComponent_GetTranslation(Entity.ID, out Vector3 translation);
+				return translation;
+			}
+			set
+			{
+				InternalCalls.TransformComponent_SetTranslation(Entity.ID, ref value);
+			}
+		}
+	}
+
+	// === 补全开始：渲染组件 ===
+	public class SpriteRendererComponent : Component
+	{
+		public Vector4 Color
+		{
+			get
+			{
+				InternalCalls.SpriteRendererComponent_GetColor(Entity.ID, out Vector4 color);
+				return color;
+			}
+			set
+			{
+				InternalCalls.SpriteRendererComponent_SetColor(Entity.ID, ref value);
+			}
+		}
+		// 如果你有 TilingFactor，也可以在这里加，前提是 InternalCalls 里有对应定义
+	}
+
+	public class CircleRendererComponent : Component
+	{
+		public Vector4 Color
+		{
+			get
+			{
+				InternalCalls.CircleRendererComponent_GetColor(Entity.ID, out Vector4 color);
+				return color;
+			}
+			set
+			{
+				InternalCalls.CircleRendererComponent_SetColor(Entity.ID, ref value);
+			}
+		}
+
+		public float Thickness
+		{
+			get => InternalCalls.CircleRendererComponent_GetThickness(Entity.ID);
+			set => InternalCalls.CircleRendererComponent_SetThickness(Entity.ID, value);
+		}
+
+		public float Fade
+		{
+			get => InternalCalls.CircleRendererComponent_GetFade(Entity.ID);
+			set => InternalCalls.CircleRendererComponent_SetFade(Entity.ID, value);
+		}
+	}
+	// === 补全结束 ===
+
+	public class CameraComponent : Component
+	{
+		// 暂时留空，只要类存在，引擎就能找到它，报错就会消失
+	}
+
+	public class ScriptComponent : Component
+	{
+		// 脚本组件通常不需要暴露太多给 C#，留空即可
+	}
+
+	public class NativeScriptComponent : Component
+	{
+		// C++ 原生脚本组件
+	}
+
+	// === 补全：物理碰撞体 ===
+	public class BoxCollider2DComponent : Component
+	{
+		// 暂时留空，防止报错
+	}
+
+	public class CircleCollider2DComponent : Component
+	{
+		// 暂时留空，防止报错
+	}
+
+	public class Rigidbody2DComponent : Component
+	{
+		public enum BodyType { Static = 0, Dynamic, Kinematic }
+
+		public Vector2 LinearVelocity
+		{
+			get
+			{
+				InternalCalls.Rigidbody2DComponent_GetLinearVelocity(Entity.ID, out Vector2 velocity);
+				return velocity;
+			}
+		}
+
+		public BodyType Type
+		{
+			get => InternalCalls.Rigidbody2DComponent_GetType(Entity.ID);
+			set => InternalCalls.Rigidbody2DComponent_SetType(Entity.ID, value);
+		}
+
+		public void ApplyLinearImpulse(Vector2 impulse, Vector2 worldPosition, bool wake)
+		{
+			InternalCalls.Rigidbody2DComponent_ApplyLinearImpulse(Entity.ID, ref impulse, ref worldPosition, wake);
+		}
+
+		public void ApplyLinearImpulse(Vector2 impulse, bool wake)
+		{
+			InternalCalls.Rigidbody2DComponent_ApplyLinearImpulseToCenter(Entity.ID, ref impulse, wake);
+		}
+	}
+
+	public class TextComponent : Component
+	{
+		public string Text
+		{
+			get => InternalCalls.TextComponent_GetText(Entity.ID);
+			set => InternalCalls.TextComponent_SetText(Entity.ID, value);
+		}
+
+		public Vector4 Color
+		{
+			get
+			{
+				InternalCalls.TextComponent_GetColor(Entity.ID, out Vector4 color);
+				return color;
+			}
+			set
+			{
+				InternalCalls.TextComponent_SetColor(Entity.ID, ref value);
+			}
+		}
+
+		public float Kerning
+		{
+			get => InternalCalls.TextComponent_GetKerning(Entity.ID);
+			set => InternalCalls.TextComponent_SetKerning(Entity.ID, value);
+		}
+
+		public float LineSpacing
+		{
+			get => InternalCalls.TextComponent_GetLineSpacing(Entity.ID);
+			set => InternalCalls.TextComponent_SetLineSpacing(Entity.ID, value);
+		}
+	}
+}
